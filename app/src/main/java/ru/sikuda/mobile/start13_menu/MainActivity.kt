98cff7eb1927b6c1613d577b613bf23b7029151f
sikuda <sikuda@yandex.ru>
2021-09-30 00:37:53 +0300

    create simple menu

--- app/src/main/java/ru/sikuda/mobile/start13_menu/MainActivity.kt
+++ app/src/main/java/ru/sikuda/mobile/start13_menu/MainActivity.kt
@@ -2,10 +2,55 @@ package ru.sikuda.mobile.start13_menu
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.*
+import android.widget.AdapterView
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        val inflater: MenuInflater = menuInflater
+        inflater.inflate(R.menu.menuaction, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        // Handle item selection
+        return when (item.itemId) {
+            R.id.item1 -> {
+                Toast.makeText(this, "Selected Item1", Toast.LENGTH_SHORT).show()
+                true
+            }
+            R.id.item2 -> {
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
+    }
+
+    override fun onCreateContextMenu(menu: ContextMenu, v: View,
+                                     menuInfo: ContextMenu.ContextMenuInfo) {
+        super.onCreateContextMenu(menu, v, menuInfo)
+        val inflater: MenuInflater = menuInflater
+        inflater.inflate(R.menu.context_menu, menu)
+    }
+
+    override fun onContextItemSelected(item: MenuItem): Boolean {
+        val info = item.menuInfo as AdapterView.AdapterContextMenuInfo
+        return when (item.itemId) {
+            R.id.item -> {
+                //editNote(info.id)
+                true
+            }
+            else -> super.onContextItemSelected(item)
+        }
+    }
+
+
+
+
 }
